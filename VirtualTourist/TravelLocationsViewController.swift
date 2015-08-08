5065e60a73801b0f8ad3e6073e45bc2d5c89f70e
Sulaiman Azhar <ssk970@gmail.com>
2015-08-08 04:53:43 +0400

    Add Edit Mode
    Selecting pins in edit mode removes it from the map view.

--- VirtualTourist/TravelLocationsViewController.swift
+++ VirtualTourist/TravelLocationsViewController.swift
@@ -9,47 +9,111 @@
 import UIKit
 import MapKit
 
-class TravelLocationsViewController: UIViewController {
+class TravelLocationsViewController: UIViewController, MKMapViewDelegate {
     
+    @IBOutlet weak var editButton: UIBarButtonItem!
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var editLabel: UILabel!
+    
+    var annotations: [MKPointAnnotation] = [MKPointAnnotation]()
 
     let touchAndHoldGesture = UILongPressGestureRecognizer()
+    
+    var editMode: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         touchAndHoldGesture.addTarget(self, action: "mapTouchAndHold:")
         mapView.addGestureRecognizer(touchAndHoldGesture)
+        mapView.delegate = self
         
     }
     
     func mapTouchAndHold(sender: UIGestureRecognizer) {
         
-        switch(touchAndHoldGesture.state) {
-        // upon releasing the finger
-        case UIGestureRecognizerState.Ended:
+        if (editMode == false) {
+        
+            switch(touchAndHoldGesture.state) {
+                // upon releasing the finger
+            case UIGestureRecognizerState.Ended:
             
-            // convert touch point to coordinate
-            var point: CGPoint = sender.locationInView(mapView)
+                // convert touch point to coordinate
+                var point: CGPoint = sender.locationInView(mapView)
+                
+                var coordinate: CLLocationCoordinate2D = mapView.convertPoint(point,
+                    toCoordinateFromView: mapView)
+                
+                // add annotation on the coordinate
+                var annotation = MKPointAnnotation()
+                annotation.coordinate = coordinate
             
-            var coordinate: CLLocationCoordinate2D = mapView.convertPoint(point,
-                toCoordinateFromView: mapView)
+                annotations.append(annotation)
+                mapView.addAnnotation(annotation)
             
-            // add annotation on the coordinate
-            var annotation = MKPointAnnotation()
-            annotation.coordinate = coordinate
+                break;
             
-            mapView.addAnnotation(annotation)
+            default:
+                // do nothing
+                break;
+            }
             
-            break;
-            
-        default:
-            // do nothing
-            break;
         }
 
     }
+    
+    // MARK: - Actions
+    
+    @IBAction func toggleEditMode(sender: AnyObject) {
+        
+        if editMode == false {
+            
+            editButton.title = "Done"
+            
+            UIView.animateWithDuration(0.2, animations: {
+                self.view.frame.origin.y -= self.editLabel.frame.height
+            })
+            
+            editMode = true
+            
+        } else {
+            
+            editButton.title = "Edit"
+            
+            UIView.animateWithDuration(0.2, animations: {
+                self.view.frame.origin.y += self.editLabel.frame.height
+            })
+            
+            editMode = false
+            
+        }
+        
+    }
+    
+    // MARK: - Map View Delegate
+    
+    func mapView(mapView: MKMapView!, didSelectAnnotationView view: MKAnnotationView!) {
+        
+        if editMode == true {
+            
+            var index : Int = 0
+            for annotation in annotations {
+                if annotation == view.annotation as! MKPointAnnotation {
+                    break;
+                } else {
+                    index++
+                }
+            }
+        
+            mapView.removeAnnotation(view.annotation)
+            annotations.removeAtIndex(index)
+            
+        }
+        
+        mapView.deselectAnnotation(view.annotation, animated: true)
 
+    }
+    
     /*
     // MARK: - Navigation
 
